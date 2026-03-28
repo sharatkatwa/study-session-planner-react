db25ed82aa0038f4418c1018aed81b052bba8e29
sharatkatwa <sjkatwa2000@gmail.com>
2026-03-29 01:09:43 +0530

    did some changeds

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <div className="max-w-3xl mx-auto">
         {/* NAV */}
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-semibold">Study Planner</h1>
+          <h1 className="text-2xl font-semibold">Stud Chad</h1>
           
           <NavLink to={isCreatePage?'/':'/create'}  className="px-4 py-2 rounded-xl bg-black text-white text-sm">
              {isCreatePage ? "← Back" : "+ New Session"}
