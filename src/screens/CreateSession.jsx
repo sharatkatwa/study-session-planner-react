41071319f05cf3937dcf48ce4d7d5f8556c4b66b
sharatkatwa <sjkatwa2000@gmail.com>
2026-03-29 00:10:09 +0530

    added some more validation to form

--- src/screens/CreateSession.jsx
+++ src/screens/CreateSession.jsx
@@ -4,36 +4,41 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import { nanoid } from "nanoid";
 function CreateSession() {
-  const { sessions,setSessions } = useContext(MyContext);
+  const { sessions, setSessions } = useContext(MyContext);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
-  
-  const navigate = useNavigate()
 
+  const navigate = useNavigate();
+  const today = new Date();
+
+  const maxDate = new Date();
+  maxDate.setFullYear(today.getFullYear() + 2);
+
+  const formattedMax = maxDate.toISOString().split("T")[0];
+  const formattedToday = today.toISOString().split("T")[0];
   //   const handleChange = (e) => {
   //     setForm({ ...form, [e.target.name]: e.target.value });
   //   };
-console.log(sessions);
+  console.log(sessions);
   const handleData = (e) => {
-
     setSessions((prev) => [
-  ...prev,
-  {
-    topic: e.topic,
-    subject: e.subject,
-    priority: e.priority,
-    duration: e.duration,
-    date: e.date,
-    id: nanoid()
-  },
-]);
-reset()
-navigate('/')
+      ...prev,
+      {
+        topic: e.topic,
+        subject: e.subject,
+        priority: e.priority,
+        duration: e.duration,
+        date: e.date,
+        id: nanoid(),
+      },
+    ]);
+    reset();
+    navigate("/");
     // setPage("list");
   };
 
@@ -68,6 +73,10 @@ navigate('/')
             value: 10,
             message: "Minimum duration is 10 minutes",
           },
+          max: {
+            value: 999,
+            message: "Max duration is 999 minutes",
+          },
         })}
         type="number"
         name="duration"
@@ -92,13 +101,24 @@ navigate('/')
 
       <input
         defaultValue={new Date().toISOString().split("T")[0]}
-        {...register("date")}
+        {...register("date", {
+          max: {
+            value: formattedMax,
+            message: "Date cannot be more than 2 years ahead",
+          },
+          min: {
+            value: formattedToday,
+            message: "Minimum date should be today",
+          },
+        })}
         type="date"
         name="date"
         // value={form.date}
         // onChange={handleChange}
         className="w-full border rounded-lg p-3 text-sm outline-none"
       />
+      {errors.date && <p className="text-red-500">{errors.date.message}</p>}
+      
 
       <button type="submit" className="w-full py-3 bg-black text-white rounded-lg text-sm">
         Save Session
